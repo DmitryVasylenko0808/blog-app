64af5e5f95977783c4cf6ba56bb4c03ff81a219d
DmitryVasylenko0808 <dvasilenko1999@gmail.com>
2024-12-17 12:51:01 +0300

    update axiosInstance

--- client/app/_lib/axiosInstance.ts
+++ client/app/_lib/axiosInstance.ts
@@ -1,5 +1,23 @@
 import axios from "axios";
+import { getSession } from "./session";
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
+
+axiosInstance.interceptors.request.use(
+  async (cfg) => {
+    const access_token = await getSession();
+    console.log("axois", access_token);
+    if (access_token) {
+      cfg.headers.Authorization = `Bearer ${access_token}`;
+    }
+
+    return cfg;
+  },
+  (err) => {
+    console.log(err);
+  }
+);
+
+export { axiosInstance };
